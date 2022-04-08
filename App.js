88b2ee39fc4bad4f34ddff861e413ebd039c08d1
robert-virtual <robertocastill63@gmail.com>
2022-04-08 16:33:39 -0600

    done

--- App.js
+++ App.js
@@ -1,20 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { CardField, StripeProvider } from "@stripe/stripe-react-native";
+import { StatusBar } from "expo-status-bar";
+import { TextInput, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  const [publishableKey, setPublishableKey] = useState("");
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <StripeProvider
+      publishableKey={publishableKey}
+    >
+      <View style={styles.container}>
+        <TextInput placeholder="Nombre" />
+        <CardField />
+        <StatusBar style="auto" />
+      </View>
+    </StripeProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
